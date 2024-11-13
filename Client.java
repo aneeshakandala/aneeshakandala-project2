baeeb853a5db25f1d8e8ac6a6dc77bff9798d91c
Aneesha Kandala <aneeshakandala@Aneeshas-MacBook-Air.local>
2024-11-13 12:05:00 -0500

    implemented functionality to created methods. attending office hours for clarification on request() and serve() methods.

--- Client.java
+++ Client.java
@@ -1,23 +1,51 @@
+import java.net.Socket;
+import java.io.*; 
+import java.net.*;
+
 public class Client{
+    protected Socket client; 
+    protected PrintWriter out;
+    protected BufferedReader in;
 
-    public Client(){
 
+    public Client(String host, int port) throws IOException {
+        client = new Socket(host, port);
+        out = new PrintWriter(client.getOutputStream());//output stream
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));//input stream 
+
+        // Socket client = new Socket("localhost", 2021);
+        // PrintWriter out = new PrintWriter(client.getOutputStream());
+        // BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
     }
 
-    public void handshake(){
-          
+    public void handshake() throws IOException{
+        System.out.println("12345");//initial handshake
+
+        String line = in.readLine();
+
+        if("handshake sent".equals(line)){
+            return;
+        }
+        else{
+            throw new IOException("couldn't handshake" + line);
+        }
     }
 
-    public void disconnect(){
-        
+    public void disconnect() throws IOException{
+        client.close();
+        in.close();
+        out.close();
     }
 
-    public void request(){
-
+    public void request(String num){
+        //what does request do?
+        //return String? void?
+        System.out.println(num);
+        return;
     }
 
-    public void getSocket(){
-
+    public Socket getSocket(){
+        return client; 
     }
 
 
--- Server.java
+++ Server.java
@@ -1,21 +1,46 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.io.*; 
+import java.net.*;
+import java.util.*;
 
 public class Server{
+    protected ServerSocket serverSocket; 
+    protected ArrayList<LocalDateTime> times;
 
-    public Server(){
+    public Server(int port) throws IOException{
+        serverSocket = new ServerSocket(port);
+        times = new ArrayList<>(getConnectedTimes());
+    }
+
+    //takes as argument the number of clients it is expected to serve per test case
+    //serve methid will have a loop for as many clients as specified in its argument
+    //after accepting a client via successful handshake, this method will process the client
+    //request in a separate(new?) thread so that the server can continue to accpet connections
+    //while these expensive factorization calculations are being performed on behalf of 
+    //various clients 
+    public void serve(int numClients) throws IOException{
+        for(int i = 0; i < numClients; i++){
+            Socket remote = serverSocket.accept();
+            times.add(LocalDateTime.now());//adding to connected time, to track?
+            //new Thread
+
+        }
 
     }
 
-    public void serve(){
-
-    }
-
-    public void getConnectedTimes(){
-
+    //returning a sorted ArrayList of LocalDateTime
+    //representing the connection time of every client 
+    public ArrayList<LocalDateTime> getConnectedTimes(){
+        return new ArrayList<>(times);
     }
 
     public void disconnect(){
-
+        try{
+            serverSocket.close();
+        } catch (IOException e){
+            System.out.println("An exception happened.");
+        }
     }
-
 
 }
